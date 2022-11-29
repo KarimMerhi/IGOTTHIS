3dc4fd36c79a79599f777703126b49eb8c835f8c
KarimMerhi <113631276+KarimMerhi@users.noreply.github.com>
2022-11-29 14:40:54 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,37 @@
+import java.util.*;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		System.out.println("What's the size?");
+		
+		Scanner in = new Scanner(System.in);
+		
+		int size = in.nextInt();
+		
+		double[] mynum = new double[size];
+		
+		for (int i = 0; i < mynum.length; i++) {
+			System.out.println("Give me a number");
+			mynum[i]=in.nextDouble();
+		}
+		
+		Arrays.sort(mynum);
+		System.out.println(mynum[0]);
+		System.out.println(mynum[mynum.length-1]);
 	}
+	
 
 }
+
+
+
+
+
+
+
+
+
+
